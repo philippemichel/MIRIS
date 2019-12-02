9341d0ab42d09efd380b9c1cba3f5c168f2f1984
Philippe MICHEL <philippe@docteur-michel.fr>
2019-12-02 18:58:36 +0100

    Comparisons cru/pasteurisé OK présentation à revoir.

--- graph.R
+++ graph.R
@@ -10,9 +10,9 @@ ttn %>%
   theme_light() + 
   theme(plot.title = element_text(size=16, face="bold"),
         axis.title.x = element_text(size=12, face="bold"),
-        axis.title.y = element_text(size=12, face="bold"),
-        axis.text.x = element_text(size=12, face="bold"),
-        axis.text.y = element_text(size=12, face="bold"),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(size=1, face="bold"),
+        axis.text.y = element_blank(),
         legend.position = "none") 
 
 
@@ -20,6 +20,43 @@ ttn %>%
 zz <- tt %>% 
   group_by(nom) %>% 
   summarise(nb = n()) %>% 
-  filter(nb > 3)
+  filter(nb > 20)
 ttn <- tt %>% 
   filter(tt$nom %in% zz$nom)
+
+
+ll <- lm(lipides~age,data=ttn)
+summary(ll)
+
+
+aa <- tt %>% 
+  mutate(id = paste0(str_sub(nom,1,2),str_sub(nom,-3)))
+
+tt %>% 
+  ggplot() +
+  aes(x = nom, y = lipides, fill =nature.lait) +
+  geom_boxplot() +
+  labs(title = "Lipides",
+       x = "patientes",
+       y = "g/L"
+  ) + 
+  theme_light() + 
+  theme(plot.title = element_text(size=16, face="bold"),
+        axis.title.x = element_text(size=12, face="bold"),
+        axis.title.y = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position = "top") 
+
+
+
+
+ttestc <- function(varx){
+aa <- tt %>% 
+  group_by(nature.lait) %>% 
+  summarise(vvx = var({{varx}},na.rm =TRUE))
+vareq <- round(aa[[1,2]]/aa[[2,2]],3)
+print(vareq)
+}
+
+zz <- t.test(tt$glucides~tt$nature.lait,var.equal = TRUE)

3ba76761e4d50a9694a56a18fc622b7ab1525730
Philippe MICHEL <philippe@docteur-michel.fr>
2019-12-02 15:33:20 +0100

    Visualisation des données dans le temps ok.

--- graph.R
+++ graph.R
@@ -1,11 +1,11 @@
-tt %>% 
+ttn %>% 
   ggplot() +
-  aes(x = age, y = glucides, color = nature.lait) +
-  geom_point() +
-  geom_smooth() +
-  labs(title = "Glucides",
+  aes(x = age, y = lipides) +
+  geom_line() +
+  facet_grid(nom~.) +
+  labs(title = "Lipides",
        x = "Âge (j)",
-       y = "mmol/L"
+       y = "g/L"
   ) + 
   theme_light() + 
   theme(plot.title = element_text(size=16, face="bold"),
@@ -13,4 +13,13 @@ tt %>%
         axis.title.y = element_text(size=12, face="bold"),
         axis.text.x = element_text(size=12, face="bold"),
         axis.text.y = element_text(size=12, face="bold"),
-        legend.position = "right") 
+        legend.position = "none") 
+
+
+
+zz <- tt %>% 
+  group_by(nom) %>% 
+  summarise(nb = n()) %>% 
+  filter(nb > 3)
+ttn <- tt %>% 
+  filter(tt$nom %in% zz$nom)
